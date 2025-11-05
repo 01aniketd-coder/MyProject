6d7f93f4ef6d271e8f5c6323fc9712e3bc0157cb
01aniketd-coder <01aniketd@gmail.com>
2025-11-05 15:40:33 +0530

    Add variables for EC2 module configuration

--- modules/ec2_module/variables.tf
+++ modules/ec2_module/variables.tf
@@ -1,1 +1,32 @@
+variable "region" {
+  description = "AWS region"
+  type        = string
+}
+
+variable "ami" {
+  description = "AMI ID for EC2 instance"
+  type        = string
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+  default     = "t2.micro"
+}
+
+variable "key_name" {
+  description = "SSH key pair name"
+  type        = string
+}
+
+variable "vm_name" {
+  description = "Name tag for EC2 instance"
+  type        = string
+}
+
+variable "security_group_name" {
+  description = "Security group name"
+  type        = string
+  default     = "default-sg"
+}
 
