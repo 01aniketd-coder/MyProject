834c1f06bed1a35dfa6369528483ae3328f1be81
01aniketd-coder <01aniketd@gmail.com>
2025-11-05 15:48:36 +0530

    Define variables for S3 bucket setup
    Added variables for S3 bucket configuration.

--- variables.tf
+++ variables.tf
@@ -1,1 +1,22 @@
+variable "bucket_name" {
+  description = "Name of the S3 bucket"
+  type        = string
+}
+
+variable "region" {
+  description = "AWS region"
+  type        = string
+}
+
+variable "acl" {
+  description = "Canned ACL (e.g. private, public-read)"
+  type        = string
+  default     = "private"
+}
+
+variable "versioning" {
+  description = "Enable versioning for bucket"
+  type        = bool
+  default     = false
+}
 
