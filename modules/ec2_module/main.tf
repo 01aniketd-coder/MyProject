aa53118340f9cc5d7fbfe91654961f9d67fec2c6
01aniketd-coder <01aniketd@gmail.com>
2025-11-05 15:43:05 +0530

    Define AWS provider and EC2 resources
    Added AWS provider, security group, and EC2 instance resources.

--- modules/ec2_module/main.tf
+++ modules/ec2_module/main.tf
@@ -1,1 +1,37 @@
+provider "aws" {
+  region = var.region
+}
+
+# Security group
+resource "aws_security_group" "vm_sg" {
+  name        = var.security_group_name
+  description = "Allow SSH inbound traffic"
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# EC2 instance
+resource "aws_instance" "vm" {
+  ami                    = var.ami
+  instance_type          = var.instance_type
+  key_name               = var.key_name
+  vpc_security_group_ids = [aws_security_group.vm_sg.id]
+
+  tags = {
+    Name = var.vm_name
+  }
+}
 
