80f892405295468fded8e752ccf4644f6f91a3b5
01aniketd-coder <01aniketd@gmail.com>
2025-11-08 17:16:52 +0530

    Revise variable descriptions and add EC2/S3 variables
    Updated descriptions for common variables and added new variables for EC2 and S3.

--- variables.tf
+++ variables.tf
@@ -1,19 +1,42 @@
+# Common variables
 variable "project_name" {
-  description = "Name of the project"
+  description = "Project name used for tagging"
   type        = string
 }
 
 variable "environment" {
-  description = "Environment (dev, test, prod)"
+  description = "Environment name (dev/stage/prod)"
   type        = string
 }
 
 variable "region" {
-  description = "AWS region to deploy resources"
+  description = "AWS region for deployment"
   type        = string
 }
 
 variable "tags" {
   description = "Common tags applied to all resources"
   type        = map(string)
+}
+
+# EC2 variables
+variable "ami" {
+  description = "AMI ID for EC2 instance"
+  type        = string
+}
+
+variable "instance_type" {
+  description = "Instance type for EC2"
+  type        = string
+}
+
+variable "key_name" {
+  description = "Key pair name for EC2"
+  type        = string
+}
+
+# S3 variables
+variable "bucket_name" {
+  description = "Globally unique bucket name"
+  type        = string
 }
