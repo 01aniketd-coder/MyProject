43cc66d43be356afd5c149c74b0905154dee357a
01aniketd-coder <01aniketd@gmail.com>
2025-11-08 15:58:08 +0530

    Add new outputs for VPC, Subnet, and Security Group
    Added outputs for VPC ID, Subnet ID, Security Group ID, and updated descriptions for existing outputs.

--- outputs.tf
+++ outputs.tf
@@ -1,19 +1,29 @@
+output "vpc_id" {
+  description = "The ID of the VPC"
+  value       = module.vpc.vpc_id
+}
+
+output "subnet_id" {
+  description = "The ID of the Subnet"
+  value       = module.subnet.subnet_id
+}
+
+output "security_group_id" {
+  description = "Security Group ID"
+  value       = module.security_group.sg_id
+}
+
 output "ec2_instance_id" {
-  value = module.ec2.instance_id
+  description = "EC2 Instance ID"
+  value       = module.ec2.instance_id
 }
 
 output "ec2_public_ip" {
-  value = module.ec2.public_ip
-}
-
-output "ec2_private_ip" {
-  value = module.ec2.private_ip
-}
-
-output "s3_bucket_name" {
-  value = module.s3.bucket_name
+  description = "EC2 Public IP Address"
+  value       = module.ec2.public_ip
 }
 
 output "s3_bucket_arn" {
-  value = module.s3.bucket_arn
+  description = "ARN of the S3 bucket"
+  value       = module.s3.bucket_arn
 }
