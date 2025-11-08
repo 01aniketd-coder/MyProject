ba68248f799f76bbe6f7032aba833c8faac310c6
01aniketd-coder <01aniketd@gmail.com>
2025-11-08 15:49:00 +0530

    Refactor S3 module variables
    Removed descriptions and defaults from existing variables and added new variables for project name, environment, and tags.

--- modules/s3_module/variables.tf
+++ modules/s3_module/variables.tf
@@ -1,22 +1,5 @@
-variable "bucket_name" {
-  description = "Name of the S3 bucket"
-  type        = string
-}
+variable "bucket_name" { type = string }
 
-variable "region" {
-  description = "AWS region"
-  type        = string
-}
-
-variable "acl" {
-  description = "Canned ACL (e.g. private, public-read)"
-  type        = string
-  default     = "private"
-}
-
-variable "versioning" {
-  description = "Enable versioning for bucket"
-  type        = bool
-  default     = false
-}
-
+variable "project_name" { type = string }
+variable "environment"  { type = string }
+variable "tags"         { type = map(string) }
